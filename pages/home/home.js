27c46cb81cf14a6a57b2ec035fca8e3d836fab7e
Naveed <08es34@gmail.com>
2015-11-30 12:20:52 +0500

    ajax test2

--- pages/home/home.js
+++ pages/home/home.js
@@ -10,7 +10,12 @@ angular.module('Home', ['ngRoute'])
 }])
 
 .controller('HomeCtrl', ['$http', '$scope', function ( $http, scope ) {
+    scope.message = "initializing...";
     $http.get("http://505.enaveed.com/web/index.php/api/food-items/get").then(function (response) {
         scope.foodItems = response.data.data;
+        scope.message = scope.message + "\n data received..";
+    }, function (response) {
+        scope.message = scope.message + "\n error occurred..";
+        scope.foodItems = response;
     });
 }]);
